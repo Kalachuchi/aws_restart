9dbac1700c66c5df00a9e4cc90968c90087b9357
Kalachuchi <ibardalozarommel@gmail.com>
2026-09-08 04:26:44 +0000

    Add Python labs from EC2

---
+++ clean_sequence.py
@@ -0,0 +1,10 @@
+import re
+
+with open('preproinsulin-seq.txt') as f:
+    text = f.read()
+
+text = text.replace("ORIGIN", "").replace("//", "")
+cleaned = re.sub("[^a-zA-Z]", "", text)
+
+print(cleaned)
+print(f"Length: {len(cleaned)}")
