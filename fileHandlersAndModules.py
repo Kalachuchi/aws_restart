42697edefdc2683c6c491f21edbf879346b3fdff
Kalachuchi <ibardalozarommel@gmail.com>
2026-09-09 03:45:53 +0000

    Lab 126

--- fileHandlersAndModules.py
+++ fileHandlersAndModules.py
@@ -1,14 +1,15 @@
 import json
 
 
+
 def readJsonFile(fileName):
     data = ""
     try:
-        with open("/home/ec2-user/aws_restart/insulin.json") as json_file:
+        with open(fileName) as json_file:
             data = json.load(json_file)
     except IOError:
         print("Coud not read file")
     return data
 
 
-print(readJsonFile("/home/ec2-user/aws_restart/insulin.json"))
+
